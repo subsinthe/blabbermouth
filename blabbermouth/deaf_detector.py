14abf79f08cdd4ca3040a2fbe346dbecf8ddb1c8
Vladimir Golubev <subsinthe@gmail.com>
2019-03-16 23:14:37 +0000

    deaf_detector: Markdown is not necessary

--- blabbermouth/deaf_detector.py
+++ blabbermouth/deaf_detector.py
@@ -125,9 +125,7 @@ class DeafDetectorHandler(telepot.aio.helper.ChatHandler):
     async def _on_chat_message(self, message):
         answer = not_none(self._backend.try_reply(message))
         await self.sender.sendMessage(
-            "_{}_".format(answer),
-            parse_mode="Markdown",
-            reply_to_message_id=message["message_id"],
+            answer, reply_to_message_id=message["message_id"]
         )
 
     def on__idle(self, _):
